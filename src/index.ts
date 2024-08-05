5599abef15fe76be9f841da196a6403e97d00808
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-08-05 08:56:26 -0400

    add stub class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import { type CreateIssueMutation } from "@linear/sdk";
 
-export type CreateIssue = Parameters<CreateIssueMutation["fetch"]>[0];
+export type Linear_CreateIssueInput = Parameters<CreateIssueMutation["fetch"]>[0];
+
+type ErrorType = "MERGE_ERROR" | "INVALID_ISSUE" | "UNKNOWN"
+
+interface LinearAutoTaskError extends Error {
+  type: ErrorType
+}
+
+export class IssueBuilder {
+  issues: Linear_CreateIssueInput[] = []
+
+  constructor(...issues: Linear_CreateIssueInput[]) {
+  }
+
+  addIssues(...issues: Linear_CreateIssueInput[]) {}
+  
+  merge(...issueBuilders: IssueBuilder[]) {}
+
+  toString(): string {
+    return ""
+  }
+}
