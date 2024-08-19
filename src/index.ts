6d1063a671b8c287a35decf8c893bebf2b5edfd3
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-08-19 11:17:37 -0400

    stub tests and impl issuebuilder class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { type CreateIssueMutation } from "@linear/sdk";
-import { getAllTimezones } from "countries-and-timezones"
+import { getAllTimezones, getTimezone } from "countries-and-timezones";
 
-type RepeatOptions = {
-  type: "daily"
-} | {
-  type: "day_of_the_week"
-  day: "Sunday" | "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday"
-} | {
-  type: "day_of_the_month"
-  day: number
-}
+type RepeatOptions =
+  | {
+      type: "daily";
+    }
+  | {
+      type: "day_of_the_week";
+      day:
+        | "Sunday"
+        | "Monday"
+        | "Tuesday"
+        | "Wednesday"
+        | "Thursday"
+        | "Friday"
+        | "Saturday";
+    }
+  | {
+      type: "day_of_the_month";
+      day: number;
+    };
 
-type TimeZone = keyof ReturnType<typeof getAllTimezones>
+type TimeZone = keyof ReturnType<typeof getAllTimezones>;
 
 export type Issue = Parameters<CreateIssueMutation["fetch"]>[0] & {
   autoTaskName: string;
@@ -19,28 +29,49 @@ export type Issue = Parameters<CreateIssueMutation["fetch"]>[0] & {
   timezone: TimeZone;
 };
 
-type ErrorType = "MERGE_ERROR" | "INVALID_ISSUE" | "UNKNOWN"
+type ErrorType = "MERGE_ERROR" | "INVALID_ISSUE" | "UNKNOWN";
 
-interface LinearAutoTaskError extends Error {
-  type: ErrorType
+export class LinearAutoTaskError extends Error {
+  constructor(type: ErrorType, message?: string) {
+    super();
+  }
+  type: ErrorType;
 }
 
 export class IssueBuilder {
-  issues: Issue[] = []
+  issues: Issue[] = [];
 
   constructor(...issues: Issue[]) {
+    this.addIssues(...issues);
+  }
+
+  _validateUniqueAutoTaskNames(...issues: Issue[]) {
+    const set = new Set(this.issues.map((issue) => issue.autoTaskName));
+    for (let issue of issues) {
+      if (set.has(issue.autoTaskName)) {
+        throw new LinearAutoTaskError(
+          "INVALID_ISSUE",
+          `Cannot add issue because existing issue already exists with autoTaskName: ${issue.autoTaskName}`
+        );
+      }
+    }
   }
 
   addIssues(...issues: Issue[]) {
+    this._validateUniqueAutoTaskNames();
+    this.issues.push(...issues);
   }
 
   merge(...issueBuilders: IssueBuilder[]) {
-
+    for (let builder of issueBuilders) {
+      const issues = builder.issues;
+      this.addIssues(...issues);
+    }
   }
 
   build(): string {
     return JSON.stringify({
-      issues: this.issues
-    })
+      issues: this.issues,
+    });
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,9 @@
-import { expect, test } from "vitest";
+import { expect, test, describe } from "vitest";
+import { IssueBuilder } from "./../src/index";
 
-test("test", () => {
-  expect(true);
+describe("IssueBuilder", () => {
+  test.todo("build() returns valid JSON");
+  test.todo("addIssues() works");
+  test.todo("addIssues() throws error if autoTaskName already exists");
+  test.todo("merge() works");
 });
